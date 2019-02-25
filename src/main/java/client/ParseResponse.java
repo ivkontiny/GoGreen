67a87874322ccbde475689d42433ad200cc93f48
Ivaylo Georgiev <i.georgiev@student.tudelft.nl>
2019-02-25 11:40:36 +0100

    Checkstyle
    Made changes so the checkstyle gives less warnings

--- src/main/java/client/ParseResponse.java
+++ src/main/java/client/ParseResponse.java
@@ -4,7 +4,7 @@ import org.json.JSONObject;
 
 public class ParseResponse {
 
-    public static String parseJson(String res){
+    public static String parseJson(String res) {
         JSONObject obj = new JSONObject(res);
         String response = obj.getString("response");
         return response;
