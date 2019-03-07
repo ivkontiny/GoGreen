712ef34f58f5cb8d11d7035f318d57c98a7d582a
Alec Nonnemaker <a.m.nonnemaker@student.tudelft.nl>
2019-03-08 00:53:09 +0100

    fixed menu buttons

--- src/main/java/GUI/ControllerHome.java
+++ src/main/java/GUI/ControllerHome.java
@@ -7,6 +7,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -20,6 +21,7 @@ public class ControllerHome implements Initializable {
 
     @FXML
     private BorderPane rootPane;
+
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
--- src/main/java/GUI/ControllerMyLog.java
+++ src/main/java/GUI/ControllerMyLog.java
@@ -1,4 +1,25 @@
 package GUI;
 
-public class ControllerMyLog {
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerMyLog implements Initializable {
+    @FXML
+    private BorderPane rootPane;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void loadHome(javafx.event.ActionEvent actionEvent) throws IOException {
+        BorderPane pane = FXMLLoader.load(getClass().getClassLoader().getResource("Home.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
 }
