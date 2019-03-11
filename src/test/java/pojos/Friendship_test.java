5b8f7d7d0c1d72ee73e67610ca7142ffb221bc97
Jeroen Looij <J.looij@student.tudelft.nl>
2019-03-11 09:20:14 +0100

    Tested the pojos classes

---
+++ src/test/java/pojos/Friendship_test.java
@@ -0,0 +1,62 @@
+package pojos;
+
+import org.junit.Test;
+import pojos.Friendship;
+import static org.junit.Assert.*;
+public class Friendship_test {
+	@Test
+	public void TestConstructerNoUsers() {
+		Friendship relation1 = new Friendship();
+		assertTrue(relation1.getReceiver() == null);
+		assertTrue(relation1.getSender() == null);
+		assertTrue(relation1.getStatus() == false);
+	}
+	@Test
+	public void TestConstructerWithUsers() {
+		String user1 = "Meepo";
+		String user2 = "Rylai";
+		Friendship relation1 = new Friendship(user1, user2);
+		assertTrue(relation1.getSender() == user1);
+		assertTrue(relation1.getReceiver() == user2);
+		assertTrue(relation1.getStatus() == false);
+	}
+	@Test
+	public void TestAcceptFriendship() {
+		String user1 = "Meepo";
+		String user2 = "Rylai";
+		Friendship relation1 = new Friendship(user1, user2);
+		relation1.setStatus(true);
+		assertTrue(relation1.getStatus() == true);
+	}
+	@Test
+	public void TestSetReceiver() {
+		String user1 = "Meepo";
+		String user2 = "Rylai";
+		Friendship relation1 = new Friendship(user1, user2);
+		relation1.setReceiver("Lanaya");
+		assertTrue(relation1.getReceiver() == "Lanaya");
+	}
+	@Test
+	public void TestSetSender() {
+		String user1 = "Meepo";
+		String user2 = "Rylai";
+		Friendship relation1 = new Friendship(user1, user2);
+		relation1.setSender("Lanaya");
+		assertTrue(relation1.getSender() == "Lanaya");
+	}
+	@Test
+	public void TestEqualsTrue() {
+		String user1 = "Meepo";
+		String user2 = "Rylai";
+		Friendship relation1 = new Friendship(user1, user2);
+		Friendship relation2 = new Friendship(user1, user2);
+		assertTrue(relation1.equals(relation2));
+	}
+	public void TestEqualsFalse() {
+		String user1 = "Meepo";
+		String user2 = "Rylai";
+		Friendship relation1 = new Friendship(user1, user2);
+		Friendship relation2 = new Friendship(user2, user1);
+		assertFalse(relation1.equals(relation2));
+	}
+}
