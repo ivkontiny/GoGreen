895585dc5ea2ff5cb03863a44830458f0f2d7b30
Ivaylo <i.georgiev@student.tudelft.nl>
2019-03-18 12:23:56 +0100

    Verified username, password and email
    Implemented verification of username, password and email.

--- src/main/java/gui/ControllerRegister.java
+++ src/main/java/gui/ControllerRegister.java
@@ -6,6 +6,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import org.apache.commons.validator.routines.EmailValidator;
 import pojos.Account;
 
 import java.net.URL;
@@ -40,11 +41,25 @@ public class ControllerRegister implements Initializable {
      * @return alert message
      */
     public String checkFields() {
-        if (email.getText().length() == 0) {
+        EmailValidator ev = EmailValidator.getInstance();
+        String mail = email.getText();
+        String user = username.getText();
+        String pass = password.getText();
+
+        if (mail.length() == 0) {
             return "Email field empty";
         }
-        if (username.getText().length() < 5) {
+        if (!ev.isValid(mail)) {
+            return "Email not valid";
+        }
+        if (user.length() <= 5) {
             return "Username too short (6 characters at least)";
+        }
+        if (!user.matches("[a-zA-Z0-9]*")) {
+            return "Username contains a forbidden character";
+        }
+        if (user.charAt(0) >= '0' && user.charAt(0) <= '9') {
+            return "Username cannot start with a digit";
         }
         if (surname.getText().length() == 0) {
             return "Surname field empty";
@@ -52,10 +67,13 @@ public class ControllerRegister implements Initializable {
         if (name.getText().length() == 0) {
             return "Name field empty";
         }
-        if (password.getText().length() < 6) {
+        if (pass.length() <= 6) {
             return "Password too short (7 characters at least)";
         }
-        if (!password.getText().equals(confirmPassword.getText())) {
+        if (!pass.matches("[a-zA-Z0-9]*")) {
+            return "Password contains a forbidden character!";
+        }
+        if (!pass.equals(confirmPassword.getText())) {
             return "The passwords do not match";
         }
         return "OK";
