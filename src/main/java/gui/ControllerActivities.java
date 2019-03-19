6fb8ce42ef1fc5882dd74940e86ef0b23a854587
Alec Nonnemaker <a.m.nonnemaker@student.tudelft.nl>
2019-03-19 22:59:31 +0100

    changed activity page to look better

--- src/main/java/gui/ControllerActivities.java
+++ src/main/java/gui/ControllerActivities.java
@@ -6,38 +6,28 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ProgressBar;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
-import pojos.Activity;
-import pojos.Category;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Date;
-import java.time.LocalDate;
 import java.util.ResourceBundle;
 
 public class ControllerActivities implements Initializable {
 
-    ObservableList<String> foodActivitieList =
-            FXCollections.observableArrayList(ActivityDb.Food.descriptions);
+
 
     ObservableList<String> transportationActivitieList = FXCollections.observableArrayList("Bike");
 
     @FXML
     private BorderPane rootPane;
     @FXML
-    private ComboBox activityBox;
-
-    @FXML
-    private Label pointsText;
+    private AnchorPane activityPane;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        activityBox.setItems(foodActivitieList);
+
     }
 
     /**
@@ -57,50 +47,6 @@ public class ControllerActivities implements Initializable {
         rootPane.getChildren().setAll(pane);
     }
 
-
-    /** Gets an activity and calculates its points.
-     *
-     * @param actionEvent the action event on which points should be calculated
-     */
-    public void inputActivity(javafx.event.ActionEvent actionEvent) {
-
-        for (int i = 0; i < ActivityDb.Food.descriptions.size(); i++) {
-            if (ActivityDb.Food.descriptions.get(i).equals(activityBox.getValue())) {
-                pointsText.setText("Points: " + ActivityDb.Food.points.get(i));
-            }
-        }
-    }
-
-    /**
-     * Adds an activity to the current user.
-     * @param actionEvent the action event on which a new activity should be added
-     */
-    public void addActivity(javafx.event.ActionEvent actionEvent) {
-        String actDesc = null;
-        int points = 0;
-
-        for (int i = 0; i < ActivityDb.Food.descriptions.size(); i++) {
-            if (ActivityDb.Food.descriptions.get(i).equals(activityBox.getValue())) {
-                actDesc = ActivityDb.Food.descriptions.get(i);
-                points = ActivityDb.Food.points.get(i);
-            }
-        }
-
-        Activity activity = new Activity(actDesc, Category.food, points,
-                Date.valueOf(LocalDate.now()), Connect.getUsername());
-
-        if (Connect.addActivity(activity)) {
-            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-            alert.setHeaderText("Activity added successfully!");
-            alert.setContentText(actDesc + " added successfully!");
-            alert.showAndWait();
-        } else {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setHeaderText("Something went wrong!");
-            alert.setContentText("Your " + actDesc + " couldn't be added!");
-            alert.showAndWait();
-        }
-    }
 
     /**
      * Loads the statistics page.
@@ -122,5 +68,21 @@ public class ControllerActivities implements Initializable {
         Connect.logOut();
         BorderPane pane = FXMLLoader.load(getClass().getClassLoader().getResource("Login.fxml"));
         rootPane.getChildren().setAll(pane);
+    }
+
+    public void loadFoodActivity() throws IOException {
+        AnchorPane foodPane = FXMLLoader.load(getClass().getClassLoader().getResource("Food.fxml"));
+        activityPane.getChildren().setAll(foodPane);
+
+    }
+
+    public void loadTransportActivity(MouseEvent mouseEvent) throws IOException {
+        AnchorPane transportPane = FXMLLoader.load(getClass().getClassLoader().getResource("Transport.fxml"));
+        activityPane.getChildren().setAll(transportPane);
+    }
+
+    public void loadEnergyActivity(MouseEvent mouseEvent) throws IOException {
+        AnchorPane energyPane = FXMLLoader.load(getClass().getClassLoader().getResource("Energy.fxml"));
+        activityPane.getChildren().setAll(energyPane);
     }
 }
