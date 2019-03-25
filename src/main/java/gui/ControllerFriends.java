3f9aab996faf82925b67bfb6d44c1fe66877a72f
Alec Nonnemaker <a.m.nonnemaker@student.tudelft.nl>
2019-03-25 10:34:46 +0100

    added a friends page

---
+++ src/main/java/gui/ControllerFriends.java
@@ -0,0 +1,75 @@
+package gui;
+
+import client.Connect;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerFriends implements Initializable {
+
+
+    @FXML
+    private BorderPane rootPane;
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    public void loadMyLog(javafx.event.ActionEvent actionEvent) throws IOException {
+        BorderPane pane = FXMLLoader.load(getClass().getClassLoader().getResource("MyLog.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
+    /**
+     * The message to be displayed on the home screen.
+     * @param root the window of the application
+     */
+    public static void welcomeMessage(Parent root) {
+        javafx.scene.control.Label welcome = (Label) root.lookup("#Welcome");
+        if (welcome != null) {
+            welcome.setText("Welcome " + Connect.getUsername());
+        }
+    }
+
+    /**
+     * Loads the activities window.
+     * @param actionEvent the action on which the activity window needs to open
+     * @throws IOException when there is a problem in the action event
+     */
+    public void loadActivities(javafx.event.ActionEvent actionEvent) throws IOException {
+        BorderPane pane = FXMLLoader.load(
+                getClass().getClassLoader().getResource("Activities.fxml"));
+
+        rootPane.getChildren().setAll(pane);
+    }
+
+    /**
+     * Loads the statistics page.
+     * @param actionEvent the action event on which the statistics page should be displayed
+     * @throws IOException when something with the action event goes wrong
+     */
+    public void loadStatistics(javafx.event.ActionEvent actionEvent) throws IOException {
+        BorderPane pane = FXMLLoader.load(
+                getClass().getClassLoader().getResource("Statistics.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+
+    /**
+     * Logs the user out.
+     * @throws IOException when something with the event went wrong
+     */
+    public void logOut() throws IOException {
+        Connect.logOut();
+        BorderPane pane = FXMLLoader.load(getClass().getClassLoader().getResource("Login.fxml"));
+        rootPane.getChildren().setAll(pane);
+    }
+}
