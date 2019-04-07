5ac9557336fca6937787416cf9aa0f62d2dc14a5
Ivaylo <i.georgiev@student.tudelft.nl>
2019-04-08 00:34:56 +0200

    Testing
    Implemented tests, 99% coverage.

--- src/test/java/services/TemperatureServiceTest.java
+++ src/test/java/services/TemperatureServiceTest.java
@@ -4,25 +4,33 @@ import database.AccountDao;
 import org.junit.Test;
 import pojos.Account;
 
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class TemperatureServiceTest {
 
     @Test
     public void testInit() {
-        Account account = new Account("test", "test", "test", "test", "test");
-        AccountDao ad = new AccountDao();
-        ad.changeDatabase("test");
-        AccountService as = new AccountService();
-        as.setDb(ad);
+        Account account = new Account("test", "test", "test"    , "test", "test");
+        //AccountDao ad = new AccountDao();
+        //ad.changeDatabase("test");
+        AccountService as = mock(AccountService.class);
+        //as.setDb(ad);
+        when(as.getAccounts()).thenReturn(new ArrayList<>());
         TemperatureService.init();
 
-        as.createAccount(account);
+        ArrayList<Account> exp = new ArrayList<>();
+        exp.add(account);
+        when(as.getAccounts()).thenReturn(exp);
         TemperatureService.init();
 
+        exp.clear();
+        account.setHasHeating(true);
+        exp.add(account);
         TemperatureService.init();
-
-        as.deleteAccount(account);
     }
 
 }
