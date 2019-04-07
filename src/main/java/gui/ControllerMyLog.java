6f3b5be7bc0f3b9d1757ca51a695affec4bb9456
Dragos Vecerdea <d.vecerdea@gmail.com>
2019-04-07 16:09:48 +0200

    checkstyle on ControllerMyLog

--- src/main/java/gui/ControllerMyLog.java
+++ src/main/java/gui/ControllerMyLog.java
@@ -1,29 +1,28 @@
 package gui;
 
-import client.Connect;
 import client.ConnectAccount;
-import client.ConnectActivity;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.*;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.effect.DropShadow;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.shape.Rectangle;
-import pojos.Account;
+
 import pojos.Activity;
 import pojos.Category;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+
 import java.net.URL;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.ResourceBundle;
@@ -37,42 +36,38 @@ public class ControllerMyLog implements Initializable {
     @FXML
     private ComboBox filter;
     @FXML
-    private Button ApplyButton;
+    private Button applyButton;
 
     private ArrayList<pojos.Activity> myActivities;
 
-    private String OPTION = null;
+    private String option = null;
 
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
-        OPTION = null;
-        ApplyButton.setText("Apply filter");
-        myActivities = ConnectAccount.getActivities();
-        ObservableList<Activity> activities = getActivity();
-        ObservableList<String> filters = FXCollections.observableArrayList();
+        option = null;
+        applyButton.setText("Apply filter");
         ArrayList<String> options = new ArrayList<>();
         options.add("food");
         options.add("transportation");
         options.add("energy");
+        ObservableList<String> filters = FXCollections.observableArrayList();
         filters.setAll(options);
         filter.setItems(filters);
-
-        for(int i = 0; i < activities.size(); i++) {
+        myActivities = ConnectAccount.getActivities();
+        ObservableList<Activity> activities = getActivity();
+        for (int i = 0; i < activities.size(); i++) {
             Activity activity = activities.get(i);
 
-            feed.getChildren().add(newLog(
+            feed.getChildren().add( newLog(
                     activity.getCategory(),
                     activity.getDescription(),
                     activity.getDate().toString(),
-                    Integer.toString(activity.getPoints())));
+                    Integer.toString( activity.getPoints())) );
         }
     }
 
     /** Sets the Feed.
-     *
-     * @param
      * @throws IOException when there is an error in the action
      */
     public void setFeed() {
@@ -80,7 +75,7 @@ public class ControllerMyLog implements Initializable {
         feed.getChildren().clear();
         ObservableList<Activity> activities = getActivity();
 
-        for(int i = 0; i < activities.size(); i++) {
+        for (int i = 0; i < activities.size(); i++) {
             Activity activity = activities.get(i);
 
             feed.getChildren().add(newLog(
@@ -139,15 +134,13 @@ public class ControllerMyLog implements Initializable {
      * @param actionEvent the action event on which the friends page should be displayed
      * @throws IOException when something with the action event goes wrong
      */
-    public void ApplyFilter(javafx.event.ActionEvent actionEvent) throws IOException {
-        if (filter.getValue().equals(OPTION)) {
-            OPTION = null;
-            ApplyButton.setText("Apply filter");
-        }
-        else
-        {
-            OPTION = (String)filter.getValue();
-            ApplyButton.setText("Remove filter");
+    public void applyFilter(javafx.event.ActionEvent actionEvent) throws IOException {
+        if (filter.getValue().equals(option)) {
+            option = null;
+            applyButton.setText("Apply filter");
+        } else {
+            option = (String)filter.getValue();
+            applyButton.setText("Remove filter");
         }
         setFeed();
     }
@@ -157,12 +150,12 @@ public class ControllerMyLog implements Initializable {
      * @param actionEvent the action event on which the friends page should be displayed
      * @throws IOException when something with the action event goes wrong
      */
-    public void ChooseFilter(javafx.event.ActionEvent actionEvent) throws IOException {
-        if (filter.getValue().equals(OPTION)) {
-            ApplyButton.setText("Remove filter");
+    public void chooseFilter(javafx.event.ActionEvent actionEvent) throws IOException {
+        if (filter.getValue().equals(option)) {
+            applyButton.setText("Remove filter");
             return;
         }
-        ApplyButton.setText("Apply filter");
+        applyButton.setText("Apply filter");
     }
 
 
@@ -170,19 +163,27 @@ public class ControllerMyLog implements Initializable {
      * Displays the activities of the current user.
      * @return an observable list containing all activities
      */
-    public ObservableList<Activity> getActivity()
-    {
+    public ObservableList<Activity> getActivity() {
         ObservableList<pojos.Activity> activities = FXCollections.observableArrayList();
         Collections.reverse(myActivities);
         for (Activity activity: myActivities) {
-            if (OPTION == null || activity.getCategory().toString().equals(OPTION)) {
+            if (option == null || activity.getCategory().toString().equals(option)) {
                 activities.add(activity);
             }
         }
         return activities;
     }
 
-    public AnchorPane newLog(Category category, String activityDescription, String date, String points) {
+    /**
+     * Creates a new log on the screen.
+     * @param category the category of the newly created log
+     * @param activityDescription the description
+     * @param date the date when the log ws issued
+     * @param points the number of points gained
+     * @return an AnchorPane
+     */
+    public AnchorPane newLog(Category category, String activityDescription,
+                             String date, String points) {
 
         AnchorPane logLayout = new AnchorPane();
         logLayout.setPrefSize(480, 100);
@@ -198,7 +199,8 @@ public class ControllerMyLog implements Initializable {
         smallRect.setLayoutY(8.0);
 
         Label logDescr = new Label(activityDescription);
-        logDescr.setStyle("-fx-text-fill: white; -fx-text-alignment: center; -fx-font-size: 22px; -fx-font-family: 'Arial Rounded MT Bold';");
+        logDescr.setStyle("-fx-text-fill: white; -fx-text-alignment: center; "
+                + "-fx-font-size: 22px; -fx-font-family: 'Arial Rounded MT Bold';");
         logDescr.setLayoutX(90.0);
         logDescr.setLayoutY(18.0);
 
@@ -208,7 +210,8 @@ public class ControllerMyLog implements Initializable {
         logDate.setLayoutY(53.0);
 
         Label logNumPoints = new Label(points);
-        logNumPoints.setStyle("-fx-font-size: 24px; -fx-alignment: center; -fx-font-family: 'Arial Rounded MT Bold';");
+        logNumPoints.setStyle("-fx-font-size: 24px; -fx-alignment: center; "
+                + "-fx-font-family: 'Arial Rounded MT Bold';");
         logNumPoints.setPrefHeight(46.0);
         logNumPoints.setPrefWidth(75.0);
         logNumPoints.setLayoutX(415.0);
@@ -219,26 +222,25 @@ public class ControllerMyLog implements Initializable {
         logPoints.setLayoutX(425.0);
         logPoints.setLayoutY(54.0);
 
-        String imageURL = new String();
+        String imageUrl = new String();
 
         if (category == Category.food) {
-            imageURL = "/Images/restaurant.png";
-        }
-        else if(category == Category.transportation) {
-            imageURL = "/Images/car.png";
-        }
-        else if(category == Category.energy) {
-            imageURL = "/Images/electricity.png";
+            imageUrl = "/Images/restaurant.png";
+        } else if (category == Category.transportation) {
+            imageUrl = "/Images/car.png";
+        } else if (category == Category.energy) {
+            imageUrl = "/Images/electricity.png";
         }
 
-        Image image = new Image(imageURL);
+        Image image = new Image(imageUrl);
         ImageView imageView = new ImageView(image);
         imageView.setFitHeight(40);
         imageView.setFitWidth(40);
         imageView.setLayoutX(18.0);
         imageView.setLayoutY(27.0);
 
-        logLayout.getChildren().addAll(bigRect, smallRect, logDescr, logDate, logNumPoints, logPoints, imageView);
+        logLayout.getChildren().addAll(bigRect, smallRect, logDescr,
+                logDate, logNumPoints, logPoints, imageView);
 
         return logLayout;
     }
